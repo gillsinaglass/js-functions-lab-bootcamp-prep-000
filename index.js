3213afb4b128b92e037b2ddaf8a809353a441542
Ryan Gill <rdgill95@gmail.com>
2018-08-15 03:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(name,holiday){
+  return `Happy ${holiday}, ${name}!`
+}
